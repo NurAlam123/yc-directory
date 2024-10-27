bede65568c739bc5a880e9215019b2d5852f2c3b
Nur Alam <nuralam.rsc@gmail.com>
2024-10-27 20:44:02 +0600

    feat: add hero section and search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query || "";
+
   return (
     <>
-      <h1 className="text-2xl">hehe</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup, <br /> connect with entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on pitches and Get Noticed in Virtual Competition
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
