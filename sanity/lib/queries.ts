5f3c36c942439d26af9cbfaa0c35737a867427e2
Nur Alam <nuralam.rsc@gmail.com>
2024-10-29 14:16:04 +0600

    feat: add startup details page

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -17,3 +17,27 @@ export const STARTUP_QUERY =
   category,
   image
 }`);
+
+export const STARTUP_VIEWS_QUERY =
+  defineQuery(`*[__type == 'startup] && _id == $id][0] {
+  _id, views
+}`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[__type == 'startup' && _id == $id][0] {
+  _id,
+  title,
+  slug,
+  _createdAt,
+  author -> {
+    _id, 
+    name,
+    image,
+    bio
+  },
+  views,
+  description,
+  category,
+  image,
+  pitch
+}`);
